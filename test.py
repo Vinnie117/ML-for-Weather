9f01d700efb2f5fa0c8c5364806529bf8e3f0959
Vinnie117 <hhang@hotmail.de>
2022-02-28 13:11:23 +0100

    Some tests for project setup

--- test.py
+++ test.py
@@ -1,1 +1,23 @@
 print("test")
+
+
+
+# create venv
+# py -3 -m venv .venv
+
+
+# activate venv
+# .venv\scripts\activate
+
+
+# temporarily change the PowerShell execution policy to allow scripts to run
+# Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process
+
+# Libs installieren
+# python -m pip install matplotlib
+
+# Liste der installierten Libs
+# pip list
+
+
+
