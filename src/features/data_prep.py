628c60b716c9566a5fe6877e69212b17ddb14811
Vinnie117 <hhang@hotmail.de>
2022-03-15 17:28:56 +0100

    Pivot table debug

--- src/features/data_prep.py
+++ src/features/data_prep.py
@@ -5,15 +5,20 @@ df = pd.read_csv(r'A:\Projects\ML-for-Weather\data\raw\test_simple.csv')
 df = df.drop(columns=['Unnamed: 0', "station_id", "dataset"])
 print(df.dtypes)
 
-df['date'] = pd.to_datetime(df['date'])
+df['date'] = df['date'].astype(str)
 print(df.dtypes)
 
 pivoted = df.pivot(index="date", columns="parameter", values="value")
 
+pivoted["date"] = pivoted.index
 
 # pivoted table -> but date is empty -> why?
 # -> only empty in Data Viewer? 
-# -> yes, Data Viewer is buggy? Csv can be written and is correct
+# -> Date column is transformed to an index -> needs to be a separate column!!!
+# or use df = df.reset_indes(level=0)
+# google: pandas unstack one column
+# what is type of date column?
+
 
 pivoted.to_csv(r'A:\Projects\ML-for-Weather\data\raw\pivoted.csv', header=True)
 
