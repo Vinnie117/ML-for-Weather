b1192ee9b17fcbb94ca25d1bd7f66af589464e21
Vinnie117 <hhang@hotmail.de>
2022-03-15 19:22:04 +0100

    cleaned data preparation

--- src/features/data_prep.py
+++ src/features/data_prep.py
@@ -1,31 +1,13 @@
 import pandas as pd
 
 # load data
-df = pd.read_csv(r'A:\Projects\ML-for-Weather\data\raw\test_simple.csv') 
-df = df.drop(columns=['Unnamed: 0', "station_id", "dataset"])
-print(df.dtypes)
+df_raw = pd.read_csv(r'A:\Projects\ML-for-Weather\data\raw\test_simple.csv') 
 
-df['date'] = df['date'].astype(str)
-print(df.dtypes)
+# prepare and clean data
+df = df_raw.drop(columns=['Unnamed: 0', "station_id", "dataset"])
+df['date'] = pd.to_datetime(df['date'])
 
-pivoted = df.pivot(index="date", columns="parameter", values="value")
+df = df.pivot(index="date", columns="parameter", values="value").reset_index()
 
-pivoted["date"] = pivoted.index
-
-# pivoted table -> but date is empty -> why?
-# -> only empty in Data Viewer? 
-# -> Date column is transformed to an index -> needs to be a separate column!!!
-# or use df = df.reset_indes(level=0)
-# google: pandas unstack one column
-# what is type of date column?
-
-
-pivoted.to_csv(r'A:\Projects\ML-for-Weather\data\raw\pivoted.csv', header=True)
-
-
-
-
-
-# Try this:
-# https://pandas.pydata.org/pandas-docs/stable/user_guide/reshaping.html
-print("END")
+# Save processed data
+df.to_csv(r'A:\Projects\ML-for-Weather\data\interim\df.csv', header=True)
