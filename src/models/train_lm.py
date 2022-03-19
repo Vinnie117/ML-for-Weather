1faf231691423a56cea5becc42522add0ff8d80e
Vinnie117 <hhang@hotmail.de>
2022-03-19 13:09:58 +0100

    file for simple lm model

--- src/models/train_lm.py
+++ src/models/train_lm.py
@@ -6,4 +6,7 @@ df_lm = pd.read_csv('A:\Projects\ML-for-Weather\data\processed\df.csv')
 
 print(df_lm.dtypes)
 
+# Start with simple naive model: explain temperature at t with temperature at t-1
+
+
 print("END")
