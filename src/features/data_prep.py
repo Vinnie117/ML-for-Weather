d8d20218eaa26a4e209641f4f08d2359834760ef
Vinnie117 <hhang@hotmail.de>
2022-03-26 11:27:24 +0100

    Pipeline: raw data input and cleaning

--- src/features/data_prep.py
+++ src/features/data_prep.py
@@ -1,5 +1,4 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
 
 # load data
 df_raw = pd.read_csv(r'A:\Projects\ML-for-Weather\data\raw\test_simple.csv') 
@@ -18,28 +17,10 @@ df['month'] =  df['timestamp'].dt.month
 df['day'] =  df['timestamp'].dt.day # is this informative? Does it connect to "month"?
 df['hour'] =  df['timestamp'].dt.hour
 
-# build lags
-df['temperature_air_mean_200_lag_1'] = df['temperature_air_mean_200'].shift(1)
-df['temperature_air_mean_200_lag_2'] = df['temperature_air_mean_200'].shift(2)
-df['cloud_cover_total_lag_1'] = df['cloud_cover_total'].shift(1)
-df['cloud_cover_total_lag_2'] = df['cloud_cover_total'].shift(2)
-df['wind_speed_lag_1'] = df['wind_speed'].shift(1)
-df['wind_speed_lag_2'] = df['wind_speed'].shift(2)
 
 # build differences (e.g. change in temperature ~ velocity of variable)
 
 # build differences in differences (e.g. change in temperature change ~ acceleration of variable)
-
-# build variable of previous day (With 50%, tomorrow's weather will be the same as today's weather)
-
-
-# Naming
-
-# Ordering
-df = df[['timestamp', 'month', 'day', 'hour', 
-'temperature_air_mean_200', 'temperature_air_mean_200_lag_1', 'temperature_air_mean_200_lag_2',
-'cloud_cover_total', 'cloud_cover_total_lag_1', 'cloud_cover_total_lag_2', 
-'wind_speed', 'wind_speed_lag_1', 'wind_speed_lag_2']]
 
 print(df.dtypes)
 print(df.shape)
--- src/features/test_pipeline.py
+++ src/features/test_pipeline.py
@@ -1,15 +1,27 @@
+from posixpath import split
+import pandas as pd
 from sklearn.pipeline import Pipeline
 from pipeline_classes import InsertLags
-from pipeline_classes import df
+#from pipeline_classes import df
 from pipeline_classes import Debugger
+from pipeline_classes import Cleaner
+
+
+# load data
+df_raw = pd.read_csv(r'A:\Projects\ML-for-Weather\data\raw\test_simple.csv') 
 
 pipe = Pipeline([
+    ("debug1", Debugger()),
+    ('clean', Cleaner()),
+    ("debug2", Debugger()),
     ("lags", InsertLags([1,2,3,24])),
-    ("debug", Debugger())
+    ("debug3", Debugger())
 ])
 
 # Pipeline creates lags and prints the data
-data = pipe.fit_transform(df)
+data = pipe.fit_transform(df_raw) 
+
+
 
 data.to_csv(r'A:\Projects\ML-for-Weather\data\processed\df.csv', header=True, index=False)
 print("END")
