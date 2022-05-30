e53f7a457df1641a67eb9eb64a7799aede842031
Vinnie117 <hhang@hotmail.de>
2022-05-30 21:08:48 +0200

    minimal tests implemented

--- tests/test_data.py
+++ tests/test_data.py
@@ -2,24 +2,22 @@ import sys
 sys.path.append('A:\Projects\ML-for-Weather\src') 
 from features.pipeline_dataprep import pd_df
 import numpy as np
-import pytest
 
-print(pd_df)
-print(type(pd_df))
 
-print('end')
-
-# Test variables 
-np.testing.assert_array_equal([1.0,2.33333,np.nan],
-                              [np.exp(0),2.33333, np.nan],
-                              err_msg='Fehler')
+# Test variables with pytest
+# np.testing.assert_array_equal([1.0,2.33333,np.nan],
+#                               [np.exp(0),2.33333, np.nan],
+#                               err_msg='Fehler')
 
 
 def inc(x):
     return x + 1
 
 
-def test_answer():
+def test_answer_right():
+    assert inc(4) == 5
+
+def test_answer_wrong():
     assert inc(3) == 5
 
 print('end')
