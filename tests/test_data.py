bbd1021f9caf452d6bcc7cce5e320d2408fc1345
Vinnie117 <hhang@hotmail.de>
2022-05-31 17:26:48 +0200

    test for velocity

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,6 +1,7 @@
 ######################## Test validity of data with Pytest ########################
 
 import sys
+from matplotlib import transforms
 
 from pandas import NA 
 sys.path.append('A:\Projects\ML-for-Weather\src') 
@@ -9,11 +10,22 @@ import numpy as np
 import pandas as pd
 
 revert_transform = pd_df['temperature_lag_1'].shift(-1)[:-1]
-revert_transform.columns = ['temperature']
+#revert_transform.columns = ['temperature']
 #print(revert_transform)
 #print(pd_df['temperature'][:-1])
 #print(type(revert_transform))
 #print(type(pd_df['temperature'][:-1]))
+
+revert_transform = pd_df['temperature_velo_1'].shift(-1)[:-1]
+original = pd_df['temperature']
+original_1 = pd_df['temperature'].diff(1)[1:]
+
+transform = pd_df['temperature_velo_1'][1:]
+
+print(original)
+print(original_1)
+print(transform)
+
 
 ###########################################
 # https://stackoverflow.com/questions/53830081/python-pandas-the-truth-value-of-a-series-is-ambiguous
@@ -25,7 +37,6 @@ def test_temperature_lag_1():
     # revert the lag and delete resulting NaN
     revert_transform = pd_df['temperature_lag_1'].shift(-1)[:-1]
     original = pd_df['temperature'][:-1]
-    
     assert (original == revert_transform).all()
 
 
@@ -34,7 +45,6 @@ def test_temperature_lag_2():
     # revert the lag and delete resulting NaN
     revert_transform = pd_df['temperature_lag_2'].shift(-2)[:-2]
     original = pd_df['temperature'][:-2]
-    
     assert (original == revert_transform).all()
 
 
@@ -43,5 +53,11 @@ def test_temperature_lag_24():
     # revert the lag and delete resulting NaN
     revert_transform = pd_df['temperature_lag_24'].shift(-24)[:-24]
     original = pd_df['temperature'][:-24]
-    
     assert (original == revert_transform).all()
+
+
+def test_temperature_velo_1():
+
+    transformed = pd_df['temperature_velo_1'][1:]
+    original_transformed = pd_df['temperature'].diff(1)[1:]
+    assert (original_transformed == transformed).all()
