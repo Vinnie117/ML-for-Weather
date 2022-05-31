a196aebc071ffa90af5c6ca3269f829c8d2da72e
Vinnie117 <hhang@hotmail.de>
2022-05-31 17:41:59 +0200

    test for lagged velocity

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,36 +1,21 @@
 ######################## Test validity of data with Pytest ########################
-
 import sys
-from matplotlib import transforms
-
-from pandas import NA 
 sys.path.append('A:\Projects\ML-for-Weather\src') 
 from features.pipeline_dataprep import pd_df
 import numpy as np
 import pandas as pd
 
-revert_transform = pd_df['temperature_lag_1'].shift(-1)[:-1]
-#revert_transform.columns = ['temperature']
-#print(revert_transform)
-#print(pd_df['temperature'][:-1])
-#print(type(revert_transform))
-#print(type(pd_df['temperature'][:-1]))
 
-revert_transform = pd_df['temperature_velo_1'].shift(-1)[:-1]
-original = pd_df['temperature']
-original_1 = pd_df['temperature'].diff(1)[1:]
 
-transform = pd_df['temperature_velo_1'][1:]
+# transformed = pd_df['temperature_velo_1_lag_1'][2:]
 
-print(original)
-print(original_1)
-print(transform)
+# original_transformed = pd_df['temperature'].diff(1).shift(1)[2:]
 
+# print(transformed)
+# print(original_transformed)
 
 ###########################################
 # https://stackoverflow.com/questions/53830081/python-pandas-the-truth-value-of-a-series-is-ambiguous
-
-# -> testen, indem falsche Werte provoziert werden!
 
 def test_temperature_lag_1():
     
@@ -60,4 +45,10 @@ def test_temperature_velo_1():
 
     transformed = pd_df['temperature_velo_1'][1:]
     original_transformed = pd_df['temperature'].diff(1)[1:]
+    assert (original_transformed == transformed).all()
+
+def test_temperature_velo_1_lag_1():
+
+    transformed = pd_df['temperature_velo_1_lag_1'][2:]
+    original_transformed = pd_df['temperature'].diff(1).shift(1)[2:]
     assert (original_transformed == transformed).all()
