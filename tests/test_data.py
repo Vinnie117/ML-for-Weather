338e838881d249dcd72854af03674e867e6931ab
Vinnie117 <hhang@hotmail.de>
2022-05-31 17:06:04 +0200

    lag tests

--- tests/test_data.py
+++ tests/test_data.py
@@ -8,43 +8,40 @@ from features.pipeline_dataprep import pd_df
 import numpy as np
 import pandas as pd
 
-
-# data = {'temperature_lag_1':[pd_df['temperature_lag_1'].shift(-1)]}
-# df = pd.DataFrame(data)
-# pd.concat([data['train'], X], axis=1)
-
-
 revert_transform = pd_df['temperature_lag_1'].shift(-1)[:-1]
 revert_transform.columns = ['temperature']
-print(revert_transform)
-print(pd_df['temperature'][:-1])
-print(type(revert_transform))
-print(type(pd_df['temperature'][:-1]))
+#print(revert_transform)
+#print(pd_df['temperature'][:-1])
+#print(type(revert_transform))
+#print(type(pd_df['temperature'][:-1]))
 
-# Unterschiedliche Zeilenlängen!
-
-
-##########################################
-# Minimal test example
-
-def inc(x):
-    return x + 1
-
-def test_answer_right():
-    assert inc(4) == 5
-
-def test_answer_wrong():
-    assert inc(3) == 5
 ###########################################
+# https://stackoverflow.com/questions/53830081/python-pandas-the-truth-value-of-a-series-is-ambiguous
+
+# -> testen, indem falsche Werte provoziert werden!
 
 def test_temperature_lag_1():
     
+    # revert the lag and delete resulting NaN
     revert_transform = pd_df['temperature_lag_1'].shift(-1)[:-1]
-    #revert_transform.columns = ['temperature']
-
     original = pd_df['temperature'][:-1]
-    #original.columns = ['temperature']
     
     assert (original == revert_transform).all()
 
-print('end')
+
+def test_temperature_lag_2():
+    
+    # revert the lag and delete resulting NaN
+    revert_transform = pd_df['temperature_lag_2'].shift(-2)[:-2]
+    original = pd_df['temperature'][:-2]
+    
+    assert (original == revert_transform).all()
+
+
+def test_temperature_lag_24():
+    
+    # revert the lag and delete resulting NaN
+    revert_transform = pd_df['temperature_lag_24'].shift(-24)[:-24]
+    original = pd_df['temperature'][:-24]
+    
+    assert (original == revert_transform).all()
