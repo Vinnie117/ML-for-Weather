d14f4b87ce5a3016d9823301ae05a8d554389198
Vinnie117 <hhang@hotmail.de>
2022-06-25 13:10:06 +0200

    Plot for 3x3 lag analysis

--- src/visuals/eda.py
+++ src/visuals/eda.py
@@ -7,35 +7,43 @@ from features.pipeline_dataprep import pd_df, train, test
 
 
 ################################################################
+temperature_lags = {}
+temperature_lags['temperature_lag_1'] = train['temperature_lag_1']
+temperature_lags['temperature_lag_2'] = train['temperature_lag_2']
+temperature_lags['temperature_lag_3'] = train['temperature_lag_3']
+temperature_lags['temperature_lag_4'] = train['temperature_lag_4']
+temperature_lags['temperature_lag_5'] = train['temperature_lag_5']
+temperature_lags['temperature_lag_6'] = train['temperature_lag_6']
+temperature_lags['temperature_lag_9'] = train['temperature_lag_9']
+temperature_lags['temperature_lag_12'] = train['temperature_lag_12']
+temperature_lags['temperature_lag_24'] = train['temperature_lag_24']
 
-x1 = train['temperature_lag_1']
-x2 = train['temperature_lag_2']
+
 y = train['temperature']
 
-print(x1.name)
 
-# plt.scatter(x1, y, s=1)
-# plt.title("Lag Analysis")
-# plt.xlabel("Temperature (°C) at time t-1")
-# plt.ylabel("Temperature (°C) at time t")
-# plt.show()
-
-# plt.scatter(x2, y, s=1)
-# plt.title("Lag Analysis")
-# plt.xlabel("Temperature (°C) at time t-2")
-# plt.ylabel("Temperature (°C) at time t")
-# plt.show()
-
-fig, axs = plt.subplots(2, 2)
+fig, axs = plt.subplots(3, 3, figsize=(9,9))
 fig.suptitle('Lag Analysis')
-axs[0, 0].scatter(x1, y, s=1)
-#axs[0, 0].set_title('Axis [0, 0]')
-axs[0, 1].scatter(x2, y, s=1)
-#axs[0, 1].set_title('Axis [0, 1]')
+axs[0, 0].scatter(temperature_lags['temperature_lag_1'], y, s=1)
+axs[0, 1].scatter(temperature_lags['temperature_lag_2'], y, s=1)
+axs[0, 2].scatter(temperature_lags['temperature_lag_3'], y, s=1)
+axs[1, 0].scatter(temperature_lags['temperature_lag_4'], y, s=1)
+axs[1, 1].scatter(temperature_lags['temperature_lag_5'], y, s=1)
+axs[1, 2].scatter(temperature_lags['temperature_lag_6'], y, s=1)
+axs[2, 0].scatter(temperature_lags['temperature_lag_9'], y, s=1)
+axs[2, 1].scatter(temperature_lags['temperature_lag_12'], y, s=1)
+axs[2, 2].scatter(temperature_lags['temperature_lag_24'], y, s=1)
 
-# Auf den Namen des der Series x1, x2, ... zugreifen!
-for i, ax in enumerate(axs.flat):
-    ax.set(xlabel='temperature_lag_{}'.format(i+1), ylabel='temperature')
+# Loop through this!
+
+# for i, ax in enumerate(axs.flat):
+#     print(i)
+#     ax.set(xlabel='A', ylabel='temperature')
+
+for i, ax in zip(range(len(temperature_lags)), axs.flat):
+    ax.set(xlabel=list(temperature_lags)[i], ylabel='temperature')
+
+#plt.subplots_adjust(left=0.1, right=0.9, top=0.9, bottom=0.1)
 
 fig.show()
 
