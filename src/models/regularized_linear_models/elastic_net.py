160707b22481248d9afe7a9e09ad1cffb0d87f12
Vinnie117 <hhang@hotmail.de>
2022-08-16 21:02:09 +0200

    Move model training to function train_elastic_net()

--- src/models/regularized_linear_models/elastic_net.py
+++ src/models/regularized_linear_models/elastic_net.py
@@ -1,15 +1,15 @@
 import pandas as pd
-import numpy as np
 from time import time
 from sklearn.linear_model import ElasticNet
-from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
-import sys 
+import sys
 sys.path.append('A:\Projects\ML-for-Weather\src')  # import from parent directory
 from models.functions import adjustedR2, eval_metrics
 import mlflow
-from joblib import dump, load
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import TimeSeriesSplit
+from config import data_config 
+from hydra import compose, initialize
+
 
 # Select data
 train = pd.read_csv(r'A:\Projects\ML-for-Weather\data\processed\train_array.csv', delimiter=',', header=0)
@@ -19,57 +19,58 @@ y_train = train.iloc[:, 0]
 X_test = test.iloc[:, 1:]
 y_test = test.iloc[:, 0]
 
-print(X_train)
-
 
 #### Train a model
 
-# set model parameters
-alpha = [0.3, 0.4, 0.5, 0.6]
-l1_ratio = [0.3, 0.4, 0.5, 0.6]
+initialize(config_path="..\..\conf", job_name="config")
+cfg = compose(config_name="config")
 
-# Specifiy splitting for Time series cross validation
-tscv = TimeSeriesSplit(n_splits = 5)
+# # This would also work:
+# import os, omegaconf
+# cfg = omegaconf.OmegaConf.load(os.path.join(os.getcwd(), "src\conf\config.yaml")) 
 
-# Hyperparameter-tuning with grid search
-parameters = {'alpha':alpha, 
-              'l1_ratio':l1_ratio} 
+def train_elastic_net(cfg: data_config):
 
-mlflow.set_experiment(experiment_name='Weather') 
+    mlflow.set_experiment(experiment_name='Weather') 
 
-# max_tuning_runs: the maximum number of child Mlflow runs created for hyperparameter search estimators
-mlflow.sklearn.autolog(max_tuning_runs=None) 
+    # max_tuning_runs: the maximum number of child Mlflow runs created for hyperparameter search estimators
+    mlflow.sklearn.autolog(max_tuning_runs=None) 
 
-with mlflow.start_run(run_name='elastic net'):
+    with mlflow.start_run(run_name='elastic net'):
 
-    # Start training the model
-    t0 = time()
-    model = ElasticNet(random_state=42)
+        # Start training the model
+        t0 = time()
+        model = ElasticNet(random_state=42)
+        
+        # Hyperparameter-tuning with grid search
+        parameters = {'alpha': cfg.elastic_net.alpha, 'l1_ratio': cfg.elastic_net.l1_ratio} 
+        # Specifiy splitting for Time series cross validation
+        tscv = TimeSeriesSplit(n_splits = cfg.cv.n_splits)
 
-    # scoring: Strategy to evaluate the performance of the cross-validated model on the test set; = None -> sklearn.metrics.r2_score 
-    lr= GridSearchCV(model, parameters, cv=tscv, scoring=None, verbose=2)
-    lr.fit(X_train, y_train)
-    duration = time() - t0
+        # scoring: Strategy to evaluate the performance of the cross-validated model on the test set; = None -> sklearn.metrics.r2_score 
+        lr= GridSearchCV(model, parameters, cv=tscv, scoring=None, verbose=2)
+        lr.fit(X_train, y_train)
+        duration = time() - t0
 
-    # automatically the model with best params
-    predicted_values = lr.predict(X_test)
+        # automatically the model with best params
+        predicted_values = lr.predict(X_test)
 
-    (rmse, mae, r2, adjusted_r2) = eval_metrics(y_test, predicted_values, X_test)
+        (rmse, mae, r2, adjusted_r2) = eval_metrics(y_test, predicted_values, X_test)
 
-    # Logging model performance to mlflow -> is only done for the best model
-    mlflow.log_param("alpha", alpha)
-    mlflow.log_param("l1_ratio", l1_ratio)
-    mlflow.log_metric("rmse", rmse)
-    mlflow.log_metric("r2", r2)
-    mlflow.log_metric("mae", mae)
-    mlflow.log_metric("adjusted_r2", adjusted_r2)
-    mlflow.log_metric('duration', duration)
-
-
+        # Logging model performance to mlflow -> is only done for the best model
+        mlflow.log_param("alpha", cfg.elastic_net.alpha)
+        mlflow.log_param("l1_ratio", cfg.elastic_net.l1_ratio)
+        mlflow.log_metric("rmse", rmse)
+        mlflow.log_metric("r2", r2)
+        mlflow.log_metric("mae", mae)
+        mlflow.log_metric("adjusted_r2", adjusted_r2)
+        mlflow.log_metric('duration', duration)
 
 
-#if __name__ == "__main__":
-print('END')
+if __name__ == "__main__":
+
+    train_elastic_net(cfg = cfg)
+    print('END')
 
 
 #### Combine gridsearch cv with mlflow ####
