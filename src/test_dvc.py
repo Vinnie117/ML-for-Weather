68ad5697dd3facd19f7301c146108a94ed26931e
Vinnie117 <hhang@hotmail.de>
2022-08-21 16:58:22 +0200

    testing dvc

--- src/test_dvc.py
+++ src/test_dvc.py
@@ -5,14 +5,14 @@ import pandas as pd
 # fd = 'A:\\Projects\\ML-for-Weather\\data_dvc\\processed\\train.csv'
 with dvc.api.open(
         r'data_dvc/processed/train.csv'
-        ) as fd:
-    print(fd)
-    train = pd.read_csv(fd, delimiter=',', header=0)
+        ) as data:
+    print(data)
+    train = pd.read_csv(data, delimiter=',', header=0)
 
 # with dvc.api.open(
 #         r'data_dvc/processed/train.csv',
 #         repo='https://github.com/Vinnie117/ML-for-Weather'
-#         ) as fd:
-#     train = pd.read_csv(fd, delimiter=',', header=0)
+#         ) as data:
+#     train = pd.read_csv(data, delimiter=',', header=0)
 
 print(train.head)
