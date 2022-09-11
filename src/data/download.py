3dd4a5976e911d6bdf8a05b7ca1f6e618425fe5d
Vinnie117 <hhang@hotmail.de>
2022-09-11 17:58:24 +0200

    dates in config file

--- src/data/download.py
+++ src/data/download.py
@@ -6,13 +6,15 @@ from wetterdienst import Wetterdienst, Resolution, Period
 from wetterdienst.provider.dwd.observation import DwdObservationDataset
 from wetterdienst.provider.dwd.observation import DwdObservationRequest, DwdObservationPeriod
 from wetterdienst import Settings
+from hydra import compose, initialize
 
 # Changing temperature units to Celsius
 Settings.si_units = false
 
 API = Wetterdienst(provider="dwd", network="observation")
 
-
+initialize(config_path="..\conf", job_name="config")
+cfg = compose(config_name="config")
 
 # Test call
 # avoid timezone issue with period=DwdObservationPeriod.HISTORICAL?
@@ -20,8 +22,8 @@ API = Wetterdienst(provider="dwd", network="observation")
 
 historical_data = DwdObservationRequest(parameter=["TEMPERATURE_AIR_MEAN_200","WIND_SPEED", "CLOUD_COVER_TOTAL"],
                             resolution="hourly",
-                            start_date="2019-01-01",  # if not given timezone defaulted to UTC
-                            end_date="2020-01-01",
+                            start_date=cfg.date.start,  # if not given timezone defaulted to UTC
+                            end_date=cfg.date.end,
                             ).filter_by_station_id(station_id=(1766))
 
 
@@ -37,5 +39,7 @@ historical_data = DwdObservationRequest(parameter=["TEMPERATURE_AIR_MEAN_200", "
 df_hist = historical_data.values.all().df
 
 print(df_hist)
+
+print("SAVE?")
 
 df_hist.to_csv(r'A:\Projects\data storage\ml_for_weather\raw\test_simple.csv', header=True, index=False)
